9e2527a9aa3c8cdbceb62a1f60255227845f3a21
Carlos <carlos@milkzoft.com>
2013-12-31 17:03:55 -0800

    config.php updated

--- config.php
+++ config.php
@@ -7,6 +7,5 @@
 
 define("SCPHP_LANGUAGE", "spanish");
 define("SCPHP_DICTIONARIES_PATH", "dictionaries/");
-define("SCPHP_URL", "http://localhost/SpellCheckerPHP/");
 define("SCPHP_DIRECTORY_SEPARATOR", "/");
 define('SCPHP_PATH', str_replace($_SERVER['DOCUMENT_ROOT'], $_SERVER['SERVER_NAME'] . '/', dirname(__FILE__)));
