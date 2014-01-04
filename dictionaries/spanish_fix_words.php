17ae512e7e101c5eecd7eb1019bcc32a40bbc002
Carlos Santana <carlos@milkzoft.com>
2014-01-04 15:25:40 -0800

    Fix words updated

--- dictionaries/spanish_fix_words.php
+++ dictionaries/spanish_fix_words.php
@@ -1,7 +1,13 @@
 <?php
 return array(
+	"angustía" => "angustia",
+	" bién " => " bien ",
+	" bién" => " bien",
+	"bohemía" => "bohemia",
+	"caricía" => "caricia",
+	"estatúa" => "estatua",
+	"llamás" => "llamas",
 	"Pure de papá" => "Pure de papa", 
 	"pure de papá" => "pure de papa",
-	" bién " => " bien ",
-	" bién" => " bien"
+	" són " => " son ",
 );
