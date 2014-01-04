0d59ab49d4ff9834b68052c2f9ffde7f9fc96db1
Carlos <carlos@milkzoft.com>
2014-01-03 16:50:38 -0800

    fixWords added

--- dictionaries/spanish_fix_words.php
+++ dictionaries/spanish_fix_words.php
@@ -1,7 +1,5 @@
 <?php
 return array(
-	"/\bPure de papá\b/" => "Pure de papa", 
-	"/\bpure de papá\b/" => "pure de papá",
-	"/\bPure de papa\b/" => "Pure de papa", 
-	"/\bpure de pap\b/" => "pure de papá",
+	"Pure de papá" => "Pure de papa", 
+	"pure de papá" => "pure de papá",
 );
