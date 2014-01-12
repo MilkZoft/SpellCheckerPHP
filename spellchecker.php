3016b4fba61eaba18ccc89b68ded13851c21a454
Carlos Santana <carlos@milkzoft.com>
2014-01-11 23:10:40 -0800

    SpellCheckerPHP

--- spellchecker.php
+++ spellchecker.php
@@ -315,7 +315,7 @@ if (!function_exists("cleanHTML")) {
 	function cleanHTML($text)
 	{
 		$text = stripslashes($text);
-		
+
 		$search = array(
 			'@<script[^>]*?>.*?</script>@si', '@<[\/\!]*?[^<>]*?>@si', '@([\r\n])[\s]+@', '@&(quot|#34);@i', '@&(amp|#38);@i', 
 			'@&(lt|#60);@i', '@&(gt|#62);@i', '@&(nbsp|#160);@i', '@&(iexcl|#161);@i', '@&(cent|#162);@i', '@&(pound|#163);@i', 
@@ -324,6 +324,6 @@ if (!function_exists("cleanHTML")) {
 		
 		$replace = array('', '', '\1', '"', '&', '<', '>', ' ', chr(161), chr(162), chr(163), chr(169), 'chr(\1)');		
 		
-		return preg_replace($search, $replace, $text);
+		return trim(preg_replace($search, $replace, $text));
 	}
 }
