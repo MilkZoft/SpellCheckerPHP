28f24f8274281716ef426a5bf94c9d3d04977c88
Carlos Santana <carlos@milkzoft.com>
2014-01-11 22:38:09 -0800

    SpellCheckerPHP

--- spellchecker.php
+++ spellchecker.php
@@ -314,14 +314,6 @@ if (!function_exists("removeChars")) {
 if (!function_exists("cleanHTML")) {
 	function cleanHTML($HTML)
 	{
-		$search = array(
-			'@<script[^>]*?>.*?</script>@si', '@<[\/\!]*?[^<>]*?>@si', '@([\r\n])[\s]+@', '@&(quot|#34);@i', '@&(amp|#38);@i', 
-			'@&(lt|#60);@i', '@&(gt|#62);@i', '@&(nbsp|#160);@i', '@&(iexcl|#161);@i', '@&(cent|#162);@i', '@&(pound|#163);@i', 
-			'@&(copy|#169);@i', '@&#(\d+);@e'
-		);		
-		
-		$replace = array('', '', '\1', '"', '&', '<', '>', ' ', chr(161), chr(162), chr(163), chr(169), 'chr(\1)');		
-		
-		return preg_replace($search, $replace, $HTML);
+		return preg_replace('/<[^<]+?>/g', ' ', $HTML);
 	}
 }
