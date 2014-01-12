e60fe92b4b3f96266d94635bd6ca2f21a3197973
Carlos Santana <carlos@milkzoft.com>
2014-01-11 23:08:03 -0800

    SpellCheckerPHP

--- api/index.php
+++ api/index.php
@@ -17,15 +17,7 @@ if (isset($_REQUEST["text"]) and strlen($_REQUEST["text"]) > 2) {
 
 		include "../spellchecker.php";
 
-		if (isset($_REQUEST["encode"])) {
-			$text = base64_decode($_REQUEST["text"]);
-
-			die(var_dump($text));
-		} else {
-			$text = $_REQUEST["text"];
-		}
-		die("no");
-		$text = spellChecker($text, $language);
+		$text = spellChecker($_REQUEST["text"], $language);
 
 		if (isset($_REQUEST["type"]) and $_REQUEST["type"] == "json") {
 			echo json_encode(
